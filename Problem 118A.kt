c2cef49c50f98e526f1c2c3783a3f03893a7f5bc
algenia <52661627+algenia@users.noreply.github.com>
2022-09-20 20:15:17 +0200

    Update and rename Problem 118A to Problem 118A.kt

--- Problem 118A.kt
+++ Problem 118A.kt
@@ -6,7 +6,7 @@ fun main() {
     val result = arrayListOf("") // create an empty array
     for (char in line.indices) { // go through string elements
         when (line[char]) { // check if element is a vowel
-            'a', 'o', 'y', 'e', 'u', 'i' -> 0 // not an elegant solution, might need improvement
+            'a', 'o', 'y', 'e', 'u', 'i' -> 0 // not an elegant solution, might need improvement (change to !in)
             else -> result.add("." + line[char]) // add non-vowels to result array
         }
     }
