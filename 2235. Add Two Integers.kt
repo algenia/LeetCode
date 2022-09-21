273958471c166f6b33e375fb3edf2864bcea1f96
algenia <52661627+algenia@users.noreply.github.com>
2022-09-21 22:12:47 +0200

    Update and rename Problem 50A.kt to 2235. Add Two Integers.kt

---
+++ 2235. Add Two Integers.kt
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/add-two-integers/
+
+class Solution {
+    fun sum(num1: Int, num2: Int): Int = num1 + num2
+}
--- Problem 50A.kt
+++
@@ -1,7 +0,0 @@
-// https://codeforces.com/problemset/problem/50/A
-// Domino piling
-
-fun main() {
-    val line = readln().split(" ") // read line from the input, save numbers only
-    println(line[0].toInt() * line[1].toInt() / 2) // print the result of div by 2
-}
