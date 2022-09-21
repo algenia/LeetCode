3f858d61514a88d87ecf55f07e5525db910b13bd
algenia <52661627+algenia@users.noreply.github.com>
2022-09-21 22:11:17 +0200

    Delete Problem 339A.kt

--- Problem 339A.kt
+++
@@ -1,9 +0,0 @@
-// https://codeforces.com/problemset/problem/339/A
-// Helpful Maths
-
-fun main() {
-    val string = readln().split("+") // read string from input, save numbers only
- 
-    print(string.sorted().joinToString("+")) //sort numbers, print with "+" as separator
-    
-}
